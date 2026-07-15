801c0875b2c5224536c9e71ac5dac12914d9da4a
chriswenzy <chrischukwuebuka85@gmail.com>
2026-07-15 13:13:27 +0100

    feat: add script to verify BFF connectivity and update robots.txt to allow api path crawling

--- app/robots.ts
+++ app/robots.ts
@@ -4,7 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      allow: ['/', '/api/app/'],
       disallow: ['/api/', '/admin/'],
     },
     sitemap: 'https://c9x.thec9group.com/sitemap.xml',
